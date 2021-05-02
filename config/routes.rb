c3849ceddd254c0f96647e4c3abd82c2dd0dfac7
ponxpon <ponponpon816@gmail.com>
2021-05-02 08:38:47 +0000

    [Add]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   devise_for :users
   resources :books do
+    resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
   resources :users,only: [:show,:index,:edit,:update]
